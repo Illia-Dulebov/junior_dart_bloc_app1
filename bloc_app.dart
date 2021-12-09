e9b5dbc90f749f77f3daf2a0831dcb553d53385b
Illia Dulebov <55345954+Illia-Dulebov@users.noreply.github.com>
2021-12-09 12:37:21 +0200

    Delete bloc_app.dart

--- bloc_app.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:bloc_app1/cart/cart.dart';
-
-class BlocApp extends MaterialApp {
-  BlocApp({Key? key}) : super(key: key, home: SomePages());
-}
