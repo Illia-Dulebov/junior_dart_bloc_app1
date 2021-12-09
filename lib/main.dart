832c0ebbe4ceaec23550ca2c9cee752553db063c
Illia Dulebov <55345954+Illia-Dulebov@users.noreply.github.com>
2021-12-09 12:47:03 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,44 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@Illia-Dulebov 
+Illia-Dulebov
+/
+junior_dart_bloc_app1
+Public
+1
+00
+Code
+Issues
+Pull requests
+1
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+junior_dart_bloc_app1/lib/main.dart
+@tsymbaliuk7
+tsymbaliuk7 added users app
+Latest commit 2d8a6f8 23 hours ago
+ History
+ 2 contributors
+@Illia-Dulebov@tsymbaliuk7
+105 lines (98 sloc)  2.79 KB
+   
+import 'package:bloc_app1/users/bloc/user/user.dart';
+import 'package:bloc_app1/users/models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bloc/bloc.dart';
 
 import 'cart/bloc_app.dart';
+import 'users/users_app.dart';
 
 part 'page_bloc.dart';
 part 'page_state.dart';
@@ -10,8 +46,15 @@ part 'page_event.dart';
 
 void main() {
   runApp(
-    BlocProvider(
-      create: (_) => MyBloc(),
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<MyBloc>(
+         create: (BuildContext context) => MyBloc(),
+        ),
+        BlocProvider<UserBloc>(
+         create: (BuildContext context) => UserBloc(userRepository: UserRepository()),
+        ),
+        ],
       child: MyApp(),
     ),
   );
@@ -24,7 +67,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => MainPage(),
         '/Illia': (context) => BlocApp(),
-        //'/Dima': (context) => PageDima(),
+        '/Dima': (context) => UsersApp(),
         //'/Anya': (context) => PageAnya(),
       },
       initialRoute: '/',
@@ -93,3 +136,16 @@ class _MainPageState extends State<MainPage> {
         ));
   }
 }
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
